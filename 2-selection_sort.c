8fd4154f478854346926ade43b6c1a50fdbdd5d5
eno007 <eno.gupe@gmail.com>
2022-08-22 03:44:41 -0700

    Task 2 and update of 2-main.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/* selection_sort - sort of an array of intigers
+ * @array: Array to be sorted
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int swap;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (i != min)
+		{
+			swap = array[i];
+			array[i] = array[min];
+			array[min] = swap;
+			print_array(array,size);
+		}
+	}
+}
