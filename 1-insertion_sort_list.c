2808655c97da3d129638b39366409b6620870c30
Donaldoo <donald.kumaraku@gmail.com>
2022-08-24 02:55:31 -0700

    Fixed compiler errors

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,6 +10,7 @@ void insertion_sort_list(listint_t **list)
 
 	if (list == NULL || *list == NULL)
 		return;
+
 	front = (*list)->next;
 	while (front != NULL)
 	{
@@ -17,14 +18,17 @@ void insertion_sort_list(listint_t **list)
 		while (front->prev != NULL && front->n < front->prev->n)
 		{
 			back = front;
+
 			if (front->next != NULL)
 				front->next->prev = front->prev;
 			front->next = front->next;
 			front->prev = front->next->prev;
-			front->next->prev = front
+			front->next->prev = front;
+
 			if (front->prev != NULL)
 				front->prev->next = back;
 			front->prev = back;
+
 			if (front->prev == NULL)
 				*list = front;
 			else
