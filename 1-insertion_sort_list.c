d139a5ecce38f31f47029fc7436a10a8b4b4fb66
eno007 <eno.gupe@gmail.com>
2022-08-24 03:26:18 -0700

    update task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,37 +6,35 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = NULL;
-	listint_t *temp = NULL;
+	listint_t *current;
+	listint_t *temp;
 
-	current = *list;
-	current = current->next;
 
 	if (list == 0)
 		return;
 
+	current = *list;
 	while (current != NULL)
 	{
 		while (current->prev != NULL && current->n < current->prev->n)
 		{
-			temp = current;
-			if (current->next != NULL)
-				current->next->prev = temp->prev;
-			current->prev->next = temp->next;
-
-			current = current->prev;
+			temp = current->next;
+			current->next = temp->next;
 			temp->prev = current->prev;
+			if (current->prev)
+				current->prev->next = temp;
+
+			if (temp->next)
+				temp->next->prev = current;
+			current->prev = temp;
 			temp->next = current;
 
-			if (current->prev != NULL)
-				current->prev->next = temp;
-			current->prev = temp;
-
-			if (temp->prev == NULL)
+			if (temp->prev)
+				current = temp->prev;
+			else
 				*list = temp;
 
 			print_list(*list);
-			current = current->prev;
 		}
 		current = current->next;
 	}
