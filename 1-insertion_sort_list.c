56f16884e332c8eaf5bc6e0381aef229c1a2f8fa
eno007 <eno.gupe@gmail.com>
2022-08-24 02:21:20 -0700

    fixing null issue with task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,10 +9,10 @@ void insertion_sort_list(listint_t **list)
 	listint_t *front, *back;
 
 	front = *list;
-	front = front->next;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL)
 		return;
+	front = front->next;
 	while (front != NULL)
 	{
 		while (front->prev != NULL && front->n < front->prev->n)
