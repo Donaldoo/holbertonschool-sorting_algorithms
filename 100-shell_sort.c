1874bdf9905e37bca6955b3d885a41cb497b9bb1
Donaldoo <donald.kumaraku@gmail.com>
2022-08-24 07:36:52 -0700

    Added task 100

---
+++ 100-shell_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array using shell sort algorithm, knuth sequence
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void shell_sort(int *array, size_t size)
+{
+	int incr, i, j, k, n = size;
+
+	if (array == NULL || size < 2)
+		return;
+
+	incr = 1;
+	while (incr <= (n - 1) / 9)
+		incr = 3 * incr + 1;
+
+	while (incr >= 1)
+	{
+		for (i = incr; i < n; i++)
+		{
+			k = array[i];
+
+			for (j = i - incr; j >= 0 && k < array[j]; j = j - incr)
+				array[j + incr] = array[j];
+			array[j + incr] = k;
+		}
+		incr = incr / 3;
+		print_array(array, size);
+	}
+}
